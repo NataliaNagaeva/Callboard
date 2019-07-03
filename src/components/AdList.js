7296769444b7b5e0b10db4481b301a313713e2d0
NATALIA-PC\Наталья <astashkinana@gmail.com>
2019-07-03 12:28:56 +0300

    Start callboard app

---
+++ src/components/AdList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdList = () => {
+  return (
+    <div
+      className="ad-list">
+      <h2>Объявление</h2>      
+    </div>
+  );
+}
+
+export default AdList;
